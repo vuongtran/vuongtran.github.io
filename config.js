e98106aebdfd623d4a1d5244f48e7acfe2d6560e
Vuong Tran <vuongtran.cg@gmail.com>
2020-08-04 22:38:59 +0700

    Update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   url: "https://lumen.netlify.com",
-  pathPrefix: "/public",
+  pathPrefix: "/vuongtran.github.io",
   title: "Blog by John Doe",
   subtitle:
     "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
